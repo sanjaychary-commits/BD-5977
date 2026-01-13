78cb89ed507cc0fa1e4ed674de007b6f25b8eb36
sanjaychary-commits <sanjaychary@medianv.com>
2026-01-13 18:07:45 +0530

    Update app.controller.ts

--- BD-5977/crud/src/app.controller.ts
+++ BD-5977/crud/src/app.controller.ts
@@ -17,7 +17,7 @@ export class AppController {
 
    @Get('/get')
   getUsers(){
-     console.log("we need")
+     console.log("we need to be edit")
       return this.Users1.getUsers()
   }
 
@@ -41,6 +41,7 @@ export class AppController {
 
   }
 }
+
 
 
 
